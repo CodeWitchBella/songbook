0097c49baf9bada2b1f93e62cec85a0a27d12624
Isabella Skořepová <isabella@skorepova.info>
2018-06-14 15:14:11 +0200

    Fix build

--- frontend/babel-plugin-remove-jsx-namespace.js
+++ frontend/babel-plugin-remove-jsx-namespace.js
@@ -5,6 +5,7 @@ module.exports = ({ types: t }) => ({
   visitor: {
     ExportNamedDeclaration(path, state) {
       if (
+        path.node.declaration &&
         path.node.declaration.type === 'TSModuleDeclaration' &&
         path.node.declaration.id.name === 'JSX'
       ) {
