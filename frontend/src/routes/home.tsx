ab4cb468d659d54227a19c7a149c80509074fa48
Isabella Skořepová <isabella@skorepova.info>
2018-06-14 15:16:23 +0200

    Add link to create song page

--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 import TagList from 'sections/tag-list/tag-list'
+import { Link } from 'react-router-dom'
 
 const Home = () => (
   <div>
+    <Link to="/new">Create song</Link>
     <TagList />
   </div>
 )
