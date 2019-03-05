b811abc8896265b9f3a0fdc3ca389f4c0e402a0e
Isabella Skořepová <isabella@skorepova.info>
2019-03-05 15:33:54 +0100

    Fix incompatibilities

--- backend/src/handler.ts
+++ backend/src/handler.ts
@@ -1,36 +1,34 @@
 import { ApolloServer } from 'apollo-server-azure-functions'
+import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 import config from './server-config'
 
 const server = new ApolloServer(config)
 
-/*export default */ server.createHandler({
+const handler = server.createHandler({
   cors: {
     origin: ['http://localhost:3000', 'https://zpevnik.skorepova.info'],
     credentials: true,
     maxAge: 3600,
   },
 })
-
-import { AzureFunction, Context, HttpRequest } from '@azure/functions'
-
 const httpTrigger: AzureFunction = async function(
   context: Context,
   req: HttpRequest,
 ): Promise<void> {
-  context.log('HTTP trigger function processed a request.')
-  const name = req.query.name || (req.body && req.body.name)
-
-  if (name) {
-    context.res = {
-      // status: 200, /* Defaults to 200 */
-      body: 'Hello ' + (req.query.name || req.body.name),
-    }
-  } else {
-    context.res = {
-      status: 400,
-      body: 'Please pass a name on the query string or in the request body',
-    }
-  }
+  return new Promise((resolve, reject) =>
+    handler(
+      {
+        ...context,
+        done: ((err, v) => {
+          if (err) reject(err)
+          else {
+            context.res = v
+            resolve()
+          }
+        }) as typeof context.done,
+      } as any,
+      req as any,
+    ),
+  )
 }
-
 export default httpTrigger
