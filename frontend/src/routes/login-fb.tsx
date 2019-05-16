af1e7d04b442b0d9415c8b0aed758c7154c02438
Isabella Skořepová <isabella@skorepova.info>
2019-05-16 17:36:10 +0200

    Handle user cancel

--- frontend/src/routes/login-fb.tsx
+++ frontend/src/routes/login-fb.tsx
@@ -2,10 +2,16 @@
 import { jsx } from '@emotion/core'
 import { errorBoundary } from 'containers/error-boundary'
 import { Link } from 'react-router-dom'
-import { useQueryParam } from 'components/use-router'
+import { useQueryParam, useHistoryChange } from 'components/use-router'
+import { useEffect } from 'react'
 
 function LoginFB() {
+  const history = useHistoryChange()
   const [param] = useQueryParam('code')
+  const [error] = useQueryParam('error')
+  useEffect(() => {
+    if (error === 'access_denied') history.replace('/')
+  })
   return (
     <div css={{ fontSize: 18, margin: '0 auto', padding: 10, maxWidth: 650 }}>
       <p>
