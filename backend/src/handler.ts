85288d115d4ef01a90a8af0a60d5da31a77b2c08
Isabella Skořepová <isabella@skorepova.info>
2019-05-16 23:20:54 +0200

    Force allow credentials

--- backend/src/handler.ts
+++ backend/src/handler.ts
@@ -3,10 +3,13 @@ import { ApolloServer } from 'apollo-server-cloud-functions'
 
 const server = new ApolloServer({ ...config, context: src => src })
 
-export const graphql = server.createHandler({
-  cors: {
-    origin: ['localhost:3000', 'https://zpevnik.skorepova.info'],
-    credentials: true,
-    maxAge: 3600,
-  },
-})
+export const graphql = (req, res) => {
+  res.set('Access-Control-Allow-Credentials', 'true')
+  return server.createHandler({
+    cors: {
+      origin: ['http://localhost:3000', 'https://zpevnik.skorepova.info'],
+      credentials: true,
+      maxAge: 3600,
+    },
+  })(req, res)
+}
