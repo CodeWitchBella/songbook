b740f1158fc37b533db608889e30c5d387d9e056
Isabella Skořepová <isabella@skorepova.info>
2020-05-29 16:44:22 +0200

    check origin

--- api/graphql.ts
+++ api/graphql.ts
@@ -13,5 +13,24 @@ const server = new ApolloServer({
 const handler = server.createHandler()
 
 export default (req: NowRequest, res: NowResponse) => {
+  const url = getFirst(req.headers['x-now-deployment-url'])
+  if (req.method !== 'GET' && !correctOrigin(origin, url)) {
+    res.status(403)
+    res.send('Forbidden, wrong origin. Got: ' + origin)
+    return
+  }
   return handler(req, res)
+}
+
+function correctOrigin(origin: string, deploymentUrl?: string) {
+  if (origin === 'https://kucharka.skorepova.info') return true
+  if (deploymentUrl && origin === 'https://' + deploymentUrl) return true
+  if (/^https:\/\/cookbook(-[a-z-]+)?\.codewitchbella\.now\.sh$/.exec(origin))
+    return true
+  return false
+}
+
+function getFirst<T>(o: undefined | T | T[]): T | undefined {
+  if (Array.isArray(o)) return o[0]
+  return o
 }
