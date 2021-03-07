f93dfff802d204085aa6a99808ddeb3dfeec7e5b
Isabella Skořepová <isabella@skorepova.info>
2021-03-07 18:48:06 +0100

    more cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -4,26 +4,23 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import ErrorBoundary from './containers/error-boundary'
 import { BrowserRouter } from 'react-router-dom'
-import Loadable from 'react-loadable'
 import App, { InjectGlobal } from './app'
 import { ServiceWorkerStatusProvider } from './components/service-worker-status'
 
 // workaround for something removing import React from 'react'
 ;(window as any).React = React
 
-Loadable.preloadReady().then(() => {
-  const app = document.getElementById('root')
-  ReactDOM.render(
-    <React.Fragment>
-      <InjectGlobal />
-      <ErrorBoundary>
-        <BrowserRouter>
-          <ServiceWorkerStatusProvider register={serviceWorker.register}>
-            <App />
-          </ServiceWorkerStatusProvider>
-        </BrowserRouter>
-      </ErrorBoundary>
-    </React.Fragment>,
-    app,
-  )
-})
+const app = document.getElementById('root')
+ReactDOM.render(
+  <React.Fragment>
+    <InjectGlobal />
+    <ErrorBoundary>
+      <BrowserRouter>
+        <ServiceWorkerStatusProvider register={serviceWorker.register}>
+          <App />
+        </ServiceWorkerStatusProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
+  </React.Fragment>,
+  app,
+)
