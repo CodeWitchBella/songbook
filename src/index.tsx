85107616be65aa3c03aa27c900b1d638546baa86
Isabella Skořepová <isabella@skorepova.info>
2021-07-20 19:23:56 +0200

    cleanup old uninstall instructions

--- src/index.tsx
+++ src/index.tsx
@@ -8,62 +8,22 @@ import Loadable from 'react-loadable'
 import App, { InjectGlobal } from './app'
 import { ServiceWorkerStatusProvider } from 'components/service-worker-status'
 
-const oldHosts = ['songbook.skorepa.info']
-const currentHost = 'zpevnik.skorepova.info'
-
-function unregister() {
-  return navigator.serviceWorker
-    .getRegistrations()
-    .then((registrations) =>
-      Promise.all(registrations.map((r) => r.unregister)),
-    )
-}
-
-let displayInstructions = false
-if ('serviceWorker' in navigator && process.env.NODE_ENV !== 'development') {
-  if (oldHosts.includes(window.location.host)) {
-    console.log('This is old host')
-    if (window.matchMedia('(display-mode: standalone)').matches) {
-      displayInstructions = true
-      document.getElementById('root')!.innerHTML = `
-      <div style="font-size: 20px; padding: 10px;">
-      Vypadá to, že že máte tento web nainstalovaný jako appku. Díky 😊<br/>
-      Jelikož jsem web přesunula na novou adresu <a href="https://${currentHost}">${currentHost}</a>
-      tak bude potřeba, abyste si appku odinstalovali a nainstalovali znova z
-      této nové adresy. Omlouvám se za nepříjemnosti, ale lepší způsob (kromě
-      zachování původní adresy) jsem bohužel nenašla. Díky za pochopení 😃
-      <br/><br/>
-      Isabella S.
-      </div>
-      `
-    } else {
-      unregister().then(() => {
-        window.location.assign(
-          `https://${currentHost}${window.location.pathname}`,
-        )
-      })
-    }
-  }
-}
-
 // workaround for something removing import React from 'react'
 ;(window as any).React = React
 
-if (!displayInstructions) {
-  Loadable.preloadReady().then(() => {
-    const app = document.getElementById('root')
-    ReactDOM.render(
-      <React.Fragment>
-        <InjectGlobal />
-        <ErrorBoundary>
-          <BrowserRouter>
-            <ServiceWorkerStatusProvider register={serviceWorker.register}>
-              <App />
-            </ServiceWorkerStatusProvider>
-          </BrowserRouter>
-        </ErrorBoundary>
-      </React.Fragment>,
-      app,
-    )
-  })
-}
+Loadable.preloadReady().then(() => {
+  const app = document.getElementById('root')
+  ReactDOM.render(
+    <React.Fragment>
+      <InjectGlobal />
+      <ErrorBoundary>
+        <BrowserRouter>
+          <ServiceWorkerStatusProvider register={serviceWorker.register}>
+            <App />
+          </ServiceWorkerStatusProvider>
+        </BrowserRouter>
+      </ErrorBoundary>
+    </React.Fragment>,
+    app,
+  )
+})
