36b83ea90b0ac67ced89e3bf94d874c6a1420501
CodeWitchBella <isabella@skorepova.info>
2021-07-24 22:40:23 +0200

    faaaa

--- src/components/themed.tsx
+++ src/components/themed.tsx
@@ -68,7 +68,7 @@ export function DarkModeProvider({
           value: !setting ? value : setting === 'dark',
           setting: (setting as any) || 'automatic',
           setSetting: (value) => {
-            setSetting(value === 'automatic' ? undefined : value)
+            setSetting(value === 'automatic' ? null : value)
             document.documentElement.classList.remove(
               'dark',
               'light',
