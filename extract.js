bc117985ae8d555c325df33e11a344b617075dd3
Isabella Skořepová <isabella@skorepova.info>
2021-08-01 12:28:37 +0200

    fix asset credits

--- extract.js
+++ extract.js
@@ -12,5 +12,5 @@ fix('./src/locales/translation-cs.json')
 fix('./src/locales/translation-en.json')
 
 function fix(fname) {
-  fs.writeFileSync(fname, JSON.stringify(require(fname), null, 2))
+  fs.writeFileSync(fname, JSON.stringify(require(fname), null, 2) + '\n')
 }
