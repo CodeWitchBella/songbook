38c8d6a7d73237d4b4a38985e12db788f8faa7ff
Isabella Skořepová <isabella@skorepova.info>
2021-08-03 12:03:59 +0200

    drop old changelog entries

--- src/routes/changelog.tsx
+++ src/routes/changelog.tsx
@@ -32,37 +32,6 @@ function ChangelogBody() {
           <ChangeBody body={lng === 'en' ? entry.en.body : entry.cz.body} />
         </ChangelogEntry>
       ))}
-      <ChangelogEntry date="2018-06-23">
-        <ChangeBody
-          body={
-            lng === 'en' ? '- Fixed print version' : '- Opravena verze pro tisk'
-          }
-        />
-      </ChangelogEntry>
-      <ChangelogEntry date="2018-06-16">
-        <ChangeBody
-          body={
-            lng === 'en'
-              ? `- First complete version
-                 - Song editing
-                   - WYSIWYG editor
-                 - Song creation
-                 - Song viewing
-                 - Printable version
-                   - With toggleable preview
-                   - Including table of contents
-                 - Works offline`.replace(/\n[ \t]+/g, '\n')
-              : `- První kompletní verze
-                 - Editace písní
-                   - WYSIWYG editor
-                 - Vytváření písní
-                 - Zobrazování písní
-                 - Tisknutelná verze
-                   - Spolu s preview a obsahem
-                 - Funkční offline`.replace(/\n[ \t]+/g, '\n')
-          }
-        />
-      </ChangelogEntry>
       <ChangelogEntry date="2016-08-03">
         <ChangeBody
           body={
