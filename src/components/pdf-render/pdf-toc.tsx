e05e97d37121205ccab194f7901b0e1f31900871
Isabella Skořepová <isabella@skorepova.info>
2022-06-29 03:54:29 +0200

    add links to pdf TOC

--- src/components/pdf-render/pdf-toc.tsx
+++ src/components/pdf-render/pdf-toc.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { PDFPage } from './pdf-page'
-import { View, Text } from './primitives'
+import { View } from './primitives'
 import { usePDFSettings } from './pdf-settings'
+const Link: typeof import('@react-pdf/renderer').Link = 'LINK' as any
 
 export function PDFToc({
   list,
   idToCounter = new Map(),
 }: {
-  list: { title: string; author: string; id: string }[]
+  list: { title: string; author: string; id: string; text: string }[]
   idToCounter?: Map<string, number>
 }) {
   const { em } = usePDFSettings()
+  let page = 1
   return (
     <PDFPage
       left={true}
@@ -30,21 +32,32 @@ export function PDFToc({
         }}
       >
         <View style={{ height: em(6.7 - 2.65), width: 0 }} />
-        {list.map((song, i) => (
-          <View
-            key={i}
-            style={{
-              maxWidth: '50%',
-              paddingBottom: em(0.1),
-              paddingRight: em(0.2),
-            }}
-          >
-            <Text style={{ fontSize: em(0.8) }}>
-              {idToCounter.get(song.id) || i + 1}. {song.title}{' '}
-              {`(${song.author})`}
-            </Text>
-          </View>
-        ))}
+        {list.map((song, i) => {
+          const link = page
+          page += song.text.split('--- page break ---').length
+          return (
+            <View
+              key={i}
+              style={{
+                maxWidth: '50%',
+                paddingBottom: em(0.1),
+                paddingRight: em(0.2),
+              }}
+            >
+              <Link
+                src={link as any}
+                style={{
+                  fontSize: em(0.8),
+                  textDecoration: 'none',
+                  color: 'black',
+                }}
+              >
+                {idToCounter.get(song.id) || i + 1}. {song.title}{' '}
+                {`(${song.author})`}
+              </Link>
+            </View>
+          )
+        })}
       </View>
     </PDFPage>
   )
