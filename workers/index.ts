c223bb5f3e0e649ef542de4794da7cba68f25478
Isabella Skořepová <isabella@skorepova.info>
2023-10-01 00:20:04 +0200

    fix logged in dev

--- workers/index.ts
+++ workers/index.ts
@@ -12,6 +12,12 @@ const worker = (await import('./src/index.js')).default
 const server = http.createServer(async (nodeReq, nodeRes) => {
   const req = await convertNodeRequest(nodeReq)
   nodeRes.appendHeader('access-control-allow-origin', 'http://localhost:5513')
+  nodeRes.setHeader('Vary', 'Origin')
+  const headers = nodeReq.headers['access-control-request-headers']
+  if (headers) nodeRes.setHeader('Access-Control-Allow-Headers', headers)
+  nodeRes.setHeader('Access-Control-Allow-Methods', '*')
+  nodeRes.setHeader('Access-Control-Max-Age', '86400')
+  nodeRes.setHeader('Access-Control-Allow-Credentials', 'true')
   if (nodeReq.method === 'OPTIONS') {
     nodeRes.statusCode = 200
     nodeRes.end()
