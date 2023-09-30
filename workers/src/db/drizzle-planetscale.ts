0f8ab5162dfaef7d324eac88a6bc40901c99454b
Isabella Skořepová <isabella@skorepova.info>
2023-09-30 22:44:30 +0200

    What do you call a fake noodle? An impasta.

--- workers/src/db/drizzle-planetscale.ts
+++ workers/src/db/drizzle-planetscale.ts
@@ -8,6 +8,12 @@ export function mkDrizzle() {
     host: process.env.DATABASE_HOST,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
+    // https://github.com/planetscale/database-js/pull/102#issuecomment-1508219636
+    // https://github.com/cloudflare/workerd/issues/698
+    fetch: ((url: string, init: RequestInit<RequestInitCfProperties>) => {
+      delete (init as any)['cache'] // Remove cache header
+      return fetch(url, init)
+    }) as any,
   })
   const db = drizzle(connection, { schema })
   return db
