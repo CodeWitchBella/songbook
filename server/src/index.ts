a24cb6c8ab9d9757cea2e94b134db43422cad5bf
Isabella Skořepová <isabella@skorepova.info>
2023-11-12 19:24:50 +0100

    init the server

---
+++ server/src/index.ts
@@ -0,0 +1,51 @@
+// @ts-check
+import { Repo } from '@automerge/automerge-repo'
+import { NodeWSServerAdapter } from '@automerge/automerge-repo-network-websocket'
+import { NodeFSStorageAdapter } from '@automerge/automerge-repo-storage-nodefs'
+import express from 'express'
+import fs from 'fs'
+import os from 'os'
+import { WebSocketServer } from 'ws'
+
+const dir = 'data'
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir)
+}
+
+var hostname = os.hostname()
+
+const socket = new WebSocketServer({ noServer: true })
+
+const PORT = process.env.PORT !== undefined ? parseInt(process.env.PORT) : 3030
+const app = express()
+app.use(express.static('public'))
+
+const config = {
+  network: [new NodeWSServerAdapter(socket)],
+  storage: new NodeFSStorageAdapter(dir),
+  peerId:
+    `storage-server-${hostname}` as import('@automerge/automerge-repo').PeerId,
+  // Since this is a server, we don't share generously — meaning we only sync documents they already
+  // know about and can ask for by ID.
+  sharePolicy: async () => false,
+}
+const serverRepo = new Repo(config)
+
+app.get('/', (req, res) => {
+  res.send(`👍 @automerge/example-sync-server is running`)
+})
+
+const server = app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
+
+server.on('upgrade', (request, socket, head) => {
+  socket.handleUpgrade(request, socket, head, (socket) => {
+    socket.emit('connection', socket, request)
+  })
+})
+
+function close() {
+  socket.close()
+  server.close()
+}
