d4179f28b539e643c8168e526a7416569128b7c3
Isabella Skořepová <isabella@skorepova.info>
2023-11-12 21:28:36 +0100

    give variables more distinct names

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,14 +14,14 @@ if (!fs.existsSync(dir)) {
 
 var hostname = os.hostname()
 
-const socket = new WebSocketServer({ noServer: true })
+const wss = new WebSocketServer({ noServer: true })
 
 const PORT = process.env.PORT !== undefined ? parseInt(process.env.PORT) : 3030
 const app = express()
 app.use(express.static('public'))
 
 const config = {
-  network: [new NodeWSServerAdapter(socket)],
+  network: [new NodeWSServerAdapter(wss)],
   storage: new NodeFSStorageAdapter(dir),
   peerId:
     `storage-server-${hostname}` as import('@automerge/automerge-repo').PeerId,
@@ -39,13 +39,13 @@ const server = app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
 
-server.on('upgrade', (request, s, head) => {
-  socket.handleUpgrade(request, s, head, (s2) => {
-    socket.emit('connection', s2, request)
+server.on('upgrade', (request, connection, head) => {
+  wss.handleUpgrade(request, connection, head, (webSocket) => {
+    wss.emit('connection', webSocket, request)
   })
 })
 
 function close() {
-  socket.close()
+  wss.close()
   server.close()
 }
