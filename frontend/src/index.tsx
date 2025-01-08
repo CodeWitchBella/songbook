f8274a3b96c3506377e4f43a90a0056fca54d996
Isabella Skořepová <isabella@skorepova.info>
2025-01-09 00:17:01 +0100

    add sentry

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/react";
 import "inter-ui/inter.css";
 import "./index.css";
 
@@ -5,6 +6,10 @@ import * as ReactDOM from "react-dom/client";
 import { Routes } from "routes/routes";
 
 import { EverythingProvider } from "./everything-provider";
+
+Sentry.init({
+  dsn: "https://5f156148cd78093fb3416b2df2a5c553@o136476.ingest.us.sentry.io/4508608240615424",
+});
 
 const app = document.getElementById("root")!;
 const root = ReactDOM.createRoot(app);
