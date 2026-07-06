a15249d4845ee261d698d5ffc59b5b11856f4140
Isabella Skořepová <isabella@skorepova.info>
2026-07-06 14:45:50 +0200

    wuff

--- songbook-layout/src/lib.rs
+++ songbook-layout/src/lib.rs
@@ -6,11 +6,9 @@ mod layout_song;
 pub use data::{Item, Layout};
 use std::io::Read;
 
-use bytes::Bytes;
 use parley::fontique::Blob;
 use parley::fontique::FallbackKey;
 use parley::fontique::FontInfoOverride;
-use woff2_patched::decode::{convert_woff2_to_ttf, is_woff2};
 
 use songbook_grammar::Song;
 
@@ -38,16 +36,14 @@ impl LayoutEngine {
             weight: None,
             axes: None,
         };
-        let info = if is_woff2(&data) {
-            let mut bytes: Bytes = data.into();
-            let ttf = convert_woff2_to_ttf(&mut bytes).unwrap();
-            self.font_cx
-                .collection
-                .register_fonts(ttf.into(), Some(info_override))
-        } else {
-            self.font_cx
-                .collection
-                .register_fonts(data.to_vec().into(), Some(info_override))
+        let ttf = match data.get(0..4) {
+            Some(b"wOF2") => wuff::decompress_woff2(&data).unwrap(),
+            Some(b"wOFF") => wuff::decompress_woff1(&data).unwrap(),
+            _ => data,
         };
+        let info = self
+            .font_cx
+            .collection
+            .register_fonts(ttf.into(), Some(info_override));
     }
 }
